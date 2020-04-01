5c2747511a5b1de0d0d26187cf735702dacabeea
宋文浩 <2990224753@qq.com>
2020-04-01 14:45:03 +0800

    初始化

---
+++ src/main/java/com/example/jwtbasesm2/Jwtbasesm2Application.java
@@ -0,0 +1,13 @@
+package com.example.jwtbasesm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwtbasesm2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwtbasesm2Application.class, args);
+    }
+
+}
